674abc3b6b9a9306f246b2ae0bed356288a28ccd
Cian O Reilly <S00187820@mail.itsligo.ie>
2019-11-04 12:15:07 +0000

    Add project files.

---
+++ CA1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int IComparable.CompareTo(object obj)
+{
+                sort s = (sort)obj;
+                return String.Compare(this.make, s.make);
+
+            }
+            Shuffle myShuffle = new Shuffle();
+
+            foreach (Shuffle s in myShuffle.Cards)
+            {
+                Console.WriteLine(s);
+            }
+           
+        }
+    }
+}
---
+++ CA1/Song.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA1
+{
+    class Song2
+    {
+        enum Genre
+        {
+            Rock,
+            Pop,
+            Dance,
+            Other,
+
+        }
+         public string title;
+         public string artist;
+         public double duration;
+
+        Genre genre;
+
+
+        public Song(string a, string t)
+        {
+            this.artist = a;
+            this.title = t;
+
+        }
+
+        public Song1(string a, string t)
+        {
+            this.artist = a;
+            this.title = t;
+
+        }
+        public Song2(string a, string t)
+        {
+            this.artist = a;
+            this.title = t;
+
+        }
+
+
+
+
+        song1("ED Sheeran", "Beatiful People", "3:15", "Pop");
+        song2("Regard", "Ride It", "3:37", "Dance");
+        song3("Tones & I", "Dance Monkey", "4:20", "Dance");
+        song4("Post Malone", "Circles", "3:25", "Pop");
+        song5("ED Sheeran", "South OF the Border", "4:26", "Other");
+
+
+    }
+}
